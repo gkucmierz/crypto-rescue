f31f38c121e203628f1b4b5277b6aaef4b99dba9
Grzegorz Kućmierz <gkucmierz@gmail.com>
2024-11-23 00:59:04 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <img class="img" src="/brain.png"/>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.img {
+  width: 200px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<script setup>
+</script>
+
+<template>
+  <div class="content">
+  <h1>Odzyskiwanie kryptowalut</h1>
+
+  <p>Studium przypadku (use cases):</p>
+
+  <p>Główne kryptowaluty BTC, ETH (inne kryptowaluty indywidualne podejście)</p>
+
+  <ul>
+    <li>utracona część mnemonika (seed)</li>
+    <li>pomieszane słowa mnemonika (np. słowa zapisane na osobnych kartkach)</li>
+    <li>stary niewspierany wallet</li>
+    <li>stary plik wallet.dat (nieznane hasło, lub potencjalne informacje na temat hasła np. same cyfry, ilość znaków hasła)</li>
+  </ul>
+
+  <p>Telegram: @gkucmierz</p>
+  </div>
+</template>
+
+<style scoped>
+.content {
+  text-align: left;
+}
+</style>
